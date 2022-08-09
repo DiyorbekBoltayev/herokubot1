4c74231e17012119795572f5bcbd5843725e3cef
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-08-09 17:50:55 +0500

    test

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 include 'Telegram.php';
 $telegram = new Telegram('5594871269:AAEiMFlohmqlRT1tlkRCkRYIFoxx3tMqJHs');
 $chat_id=$telegram->ChatID();
-
-$content = array('chat_id' => $chat_id, 'text' => 'Bot ishlab chiqarilish jarayonida iltimos keyinroq qayta urinib ko\'ring');
+$text=$telegram->Text();
+$content = array('chat_id' => $chat_id, 'text' => 'Bot ishlab chiqarilish jarayonida 
+iltimos keyinroq qayta urinib ko\'ring. Siz yuborgan matn :'.$text);
 $result=$telegram->sendMessage($content);
 echo "ishladi";
 ?>
