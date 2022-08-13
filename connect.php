6f046a1f0d5e4b9e0ac025e128aadde55234cefb
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-08-13 18:22:35 +0500

    connect databese

--- connect.php
+++ connect.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "https://us-cdbr-east-06.cleardb.net/";
+$servername = "us-cdbr-east-06.cleardb.net";
 $username = "b920520e2d5cfd";
-$password = "53cac20c ";
+$password = "53cac20c";
+$db="heroku_6ecbfd4e77df4ab";
+
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect("$servername", "$username", "$password","$db");
 
 // Check connection
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    echo "Connect error";
 }
 echo "Connected successfully";
 
