48d6d47d3656a38e4dc62c580b9551c9c8701b19
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-08-14 11:45:08 +0500

    bo'laklarga ajratildi

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-
-
 include 'Telegram.php';
-$telegram = new Telegram('5594871269:AAEiMFlohmqlRT1tlkRCkRYIFoxx3tMqJHs');
+ $telegram = new Telegram('5594871269:AAEiMFlohmqlRT1tlkRCkRYIFoxx3tMqJHs');
 $chat_id=$telegram->ChatID();
 $text=$telegram->Text();
 $data=$telegram->getData();
@@ -14,11 +12,44 @@ $message=$data['message'];
 //    'text'=>$d
 //];
 //$telegram->sendMessage($content);
+$massa=[
+    '0.5 kilogramm - 💵 50 000 so`m',
+    '1 kilogramm - 💵 90 000 so`m',
+    '2 kilogramm - 💵 170 000 so`m',
+    '3 kilogramm - 💵 250 000 so`m',
+    '5 kilogramm - 💵 400 000 so`m',
+    '10 kilogramm - 💵 750 000 so`m'
+];
 
 if($text=='/start'){
+    start();
+}elseif ($text=='📜 Biz haqimizda'){
+    bizHaqimizda();
+}
+elseif ($text=='🚛 Buyurtma berish'){
+    buyurtmaBerish();
+}
+elseif ($text==$massa[0]
+    || $text==$massa[1]
+    || $text==$massa[2]
+    || $text==$massa[3]
+    || $text==$massa[4]
+    || $text==$massa[5]){
+    massaTanlandi();
+}
+elseif (file_get_contents('step.txt')=="phone"){
+    telefonYuborildi();
+}
+elseif (file_get_contents('step.txt')=='location' || $text=="🚘 O'zim boraman"){
+    buyurtmaQabulQilindi();
+}
+echo "nice2";
+
+function start(){
+    global $telegram,$chat_id;
     $option=[
-      [$telegram->buildKeyboardButton('📜 Biz haqimizda')],
-      [$telegram->buildKeyboardButton('🚛 Buyurtma berish')],
+        [$telegram->buildKeyboardButton('📜 Biz haqimizda')],
+        [$telegram->buildKeyboardButton('🚛 Buyurtma berish')],
     ];
     $keyboard=$telegram->buildKeyBoard($option, $onetime=false , $resize=true);
     $content=[
@@ -28,8 +59,9 @@ if($text=='/start'){
 
     ];
     $telegram->sendMessage($content);
-
-}elseif ($text=='📜 Biz haqimizda'){
+}
+function bizHaqimizda(){
+    global $telegram,$chat_id;
     $content=[
         'chat_id'=>$chat_id,
         'text'=>"Biz haqimizda bilib oling <a href='https://telegra.ph/Biz-haqimizda-08-10'>Link</a> "
@@ -37,15 +69,15 @@ if($text=='/start'){
     ];
     $telegram->sendMessage($content);
 }
-elseif ($text=='🚛 Buyurtma berish'){
-
+function buyurtmaBerish(){
+global $telegram,$chat_id,$massa;
     $option=[
-      [$telegram->buildKeyboardButton('0.5 kilogramm - 💵 50 000 so`m')],
-      [$telegram->buildKeyboardButton('1 kilogramm - 💵 90 000 so`m')],
-      [$telegram->buildKeyboardButton('2 kilogramm - 💵 170 000 so`m')],
-      [$telegram->buildKeyboardButton('3 kilogramm - 💵 250 000 so`m')],
-      [$telegram->buildKeyboardButton('5 kilogramm - 💵 400 000 so`m')],
-      [$telegram->buildKeyboardButton('10 kilogramm - 💵 750 000 so`m')],
+        [$telegram->buildKeyboardButton($massa[0])],
+        [$telegram->buildKeyboardButton( $massa[1])],
+        [$telegram->buildKeyboardButton( $massa[2])],
+        [$telegram->buildKeyboardButton( $massa[3])],
+        [$telegram->buildKeyboardButton( $massa[4])],
+        [$telegram->buildKeyboardButton($massa[5])],
     ];
     $keyboard=$telegram->buildKeyBoard($option,$onetime=true,$resize=true);
     $content=[
@@ -56,15 +88,10 @@ elseif ($text=='🚛 Buyurtma berish'){
     ];
     $telegram->sendMessage($content);
 }
-elseif ($text=='0.5 kilogramm - 💵 50 000 so`m'
-    || $text=='1 kilogramm - 💵 90 000 so`m'
-    || $text=='2 kilogramm - 💵 170 000 so`m'
-    || $text=='3 kilogramm - 💵 250 000 so`m'
-    || $text=='5 kilogramm - 💵 400 000 so`m'
-    || $text=='10 kilogramm - 💵 750 000 so`m'
-){
+function massaTanlandi(){
+    global $telegram,$chat_id;
     $option=[
-      [$telegram->buildKeyboardButton('📱 Telefon raqamni yuborish',$request_contact=true)]
+        [$telegram->buildKeyboardButton('📱 Telefon raqamni yuborish',$request_contact=true)]
     ];
     $keyboard=$telegram->buildKeyBoard($option,$onetime=true,$resize=true);
     $content=[
@@ -75,55 +102,52 @@ elseif ($text=='0.5 kilogramm - 💵 50 000 so`m'
     $telegram->sendMessage($content);
     file_put_contents('step.txt','phone');
 
-
 }
-elseif (file_get_contents('step.txt')=="phone"){
+function telefonYuborildi(){
+    global $message,$text;
     if($message['contact']['phone_number'] == ""){
         $phone=substr($text,1);
         if(is_numeric($phone)){
-            $option=[
-                [$telegram->buildKeyboardButton("🚚 Yetkazib berilsin",$request_contact=false,$request_location=true)],
-                [$telegram->buildKeyboardButton("🚘 O'zim boraman")]
-            ];
-            $keyboard=$telegram->buildKeyBoard($option,$onetime=true,$resize=true);
-            $content=[
-                'chat_id'=>$chat_id,
-                'reply_markup'=>$keyboard,
-                'text'=>"  🗺 Urganch tumani bo'ylab yetkazib berish bepul !\n 🏢 Bizning manzil: Urganch tumani Kattabog' mahallasi Ummon ko'chasi 28-uy"
-            ];
-            $telegram->sendMessage($content);
-            file_put_contents('step.txt','location');
-        }
-        else{
-            $option=[
-                [$telegram->buildKeyboardButton('📱 Telefon raqamni yuborish',$request_contact=true)]
-            ];
-            $keyboard=$telegram->buildKeyBoard($option,$onetime=true,$resize=true);
-            $content=[
-                'chat_id'=>$chat_id,
-                'reply_markup'=>$keyboard,
-                'text'=>"Telefon raqamini kirtishda xatolik , iltimos qaytadan  kiriting, masalan: 883621700"
-            ];
-            $telegram->sendMessage($content);
-            file_put_contents('step.txt','phone');
+            joylashuvYuborish();
+        }else{
+            telefonXato();
         }
     } else{
+        joylashuvYuborish();
+    }
 
+}
+function joylashuvYuborish(){
+global $telegram,$chat_id;
     $option=[
         [$telegram->buildKeyboardButton("🔻 Joylashuvni yuborish",$request_contact=false,$request_location=true)],
         [$telegram->buildKeyboardButton("🚘 O'zim boraman")]
     ];
     $keyboard=$telegram->buildKeyBoard($option,$onetime=true,$resize=true);
     $content=[
-      'chat_id'=>$chat_id,
+        'chat_id'=>$chat_id,
         'reply_markup'=>$keyboard,
         'text'=>"  🗺 Urganch tumani bo'ylab yetkazib berish bepul !\n🚛 Yetkazib berish uchun manzilni kiriting yoki joylashuvni yuboring. Istasangiz o'zingiz kelib olib ketishingiz ham mumkin. \n 🏢 Bizning manzil: Urganch tumani Kattabog' mahallasi Ummon ko'chasi 28-uy"
     ];
     $telegram->sendMessage($content);
     file_put_contents('step.txt','location');
 }
+function telefonXato(){
+    global $telegram,$chat_id;
+    $option=[
+        [$telegram->buildKeyboardButton('📱 Telefon raqamni yuborish',$request_contact=true)]
+    ];
+    $keyboard=$telegram->buildKeyBoard($option,$onetime=true,$resize=true);
+    $content=[
+        'chat_id'=>$chat_id,
+        'reply_markup'=>$keyboard,
+        'text'=>"Telefon raqamini kirtishda xatolik , iltimos qaytadan  kiriting, masalan: 883621700"
+    ];
+    $telegram->sendMessage($content);
+    file_put_contents('step.txt','phone');
 }
-elseif (file_get_contents('step.txt')=='location' || $text=="🚘 O'zim boraman"){
+function buyurtmaQabulQilindi(){
+    global $telegram,$chat_id;
     $content=[
         'chat_id'=>$chat_id,
         'text'=>"  ✅ Buyurtma qabul qilindi.\n☎️ Siz bilan tez orada bog'lanamiz."
@@ -131,4 +155,3 @@ elseif (file_get_contents('step.txt')=='location' || $text=="🚘 O'zim boraman"
     $telegram->sendMessage($content);
     file_put_contents('step.txt','tugadi');
 }
-echo "nice";
